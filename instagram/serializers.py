328b18185a6745a5f48a08e5baf521a0b8ba35c2
youme <mrsimplelife@naver.com>
2021-03-11 14:54:15 +0900

    Add author to posts api

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,8 +1,19 @@
 from instagram.models import Post
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "name", "avatar_url"]
 
 
 class PostSerializer(ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Post
         fields = "__all__"
