29efb3c3f4d3cbd74098331f947423e19147160f
youme <mrsimplelife@naver.com>
2021-03-11 18:35:35 +0900

    Fix bug

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
@@ -21,6 +22,16 @@ class SignupSerializer(ModelSerializer):
 
 
 class SuggestionUserSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField("avatar_url_field")
+
+    def avatar_url_field(self, author):
+        if re.match(r"^https?://", author.avatar_url):
+            return author.avatar_url
+
+        scheme = self.context["request"].scheme
+        host = self.context["request"].get_host()
+        return scheme + "://" + host + author.avatar_url
+
     class Meta:
         model = User
         fields = ["username", "name", "avatar_url"]
